24f3b81759d81e128ad709c5808ab6aff45a281b
rashidalam9678 <mohdrashidalam786@gmail.com>
2022-01-13 20:56:45 +0530

    some intial file has been added

---
+++ Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h4>this is h4</h4>
+        </div>
+    )
+}
+
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import Header from '../Components/Header'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Header/>
+    </>
+  )
 }
 
 export default MyApp
